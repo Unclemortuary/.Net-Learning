bd09e7e1327f59c5dfac7b3eb98e3f2645abaad9
Артем Гусев <artem_mihalych@mail.ru>
2017-11-15 00:11:26 +0300

    File upload on server(web api)

---
+++ ASP.NET/WebAPIFilesUpload/WebAPIFilesUpload/Controllers/UploadController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.IO;
+
+namespace WebAPIFilesUpload.Controllers
+{
+    public class UploadController : ApiController
+    {
+
+
+        public async Task<IHttpActionResult> Post()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                return BadRequest();
+            }
+            var provider = new MultipartMemoryStreamProvider();
+            // путь к папке на сервере
+            string root = System.Web.HttpContext.Current.Server.MapPath("~/App_Data");
+            await Request.Content.ReadAsMultipartAsync(provider);
+
+            foreach (var file in provider.Contents)
+            {
+                var filename = file.Headers.ContentDisposition.FileName.Trim('\"');
+                byte[] fileArray = await file.ReadAsByteArrayAsync();
+
+                using (FileStream fs = new FileStream(root + "\\" + filename, FileMode.Create))
+                {
+                    await fs.WriteAsync(fileArray, 0, fileArray.Length);
+                }
+
+            }
+            return Ok("Files uploaded");
+        }
+
+
+        //public async Task<IHttpActionResult> PostFormData()
+        //{
+        //    // Check if the request contains multipart/form-data.
+        //    if (!Request.Content.IsMimeMultipartContent())
+        //    {
+        //        throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+        //    }
+
+        //    string root = HttpContext.Current.Server.MapPath("~/App_Data");
+        //    var provider = new MultipartFormDataStreamProvider(root);
+
+        //    try
+        //    {
+        //        // Read the form data.
+        //        await Request.Content.ReadAsMultipartAsync(provider);
+
+        //        // This illustrates how to get the file names.
+        //        foreach (MultipartFileData file in provider.FileData)
+        //        {
+        //            Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+        //            Trace.WriteLine("Server file path: " + file.LocalFileName);
+        //        }
+        //        return Ok("Files uploaded");
+        //    }
+        //    catch (System.Exception e)
+        //    {
+        //        return InternalServerError(e);
+        //    }
+        //}
+    }
+}
