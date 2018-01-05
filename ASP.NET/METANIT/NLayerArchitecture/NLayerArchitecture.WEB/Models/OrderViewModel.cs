61d9db179c4ae5c1de8fc21fa1e0203d1cfb3430
Unclemortuary <artem_mihalych@mail.ru>
2018-01-05 22:28:20 +0300

    HelloWorld for ReactJS(from NuGet) as a simple Comment Service(ONLY)

--- ASP.NET/METANIT/NLayerArchitecture/NLayerArchitecture.WEB/Models/OrderViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NLayerArchitecture.WEB.Models
-{
-    public class OrderViewModel
-    {
-        public int PhoneId { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
-    }
-}
---
+++ ASP.NET/ReactJS.HelloWorld/ReactJS.HelloWorld/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactJS.HelloWorld.MvcApplication" Language="C#" %>
